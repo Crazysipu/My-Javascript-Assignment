5890c972e0d4c0ad2cfa50cb064be07a69187506
Harisankar <harisankarsahoo58@gmail.com>
2022-03-06 15:28:19 +0530

    creatObject

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,19 +1,18 @@
-function CanDrive(hasDrivingLiscence, isTired, isSober) {
+function CanDrive(hasDrivingLiscence, isTired, isSober) {// true,true,false
     // Write you code here
-    if (hasDrivingLiscence){
-        
-        if(isTired==true){
-            return "You shouldn't drive"
+    if(hasDrivingLiscence){// hasdrivingliscence=true
+        if(isTired===true){//istired=true
+            return "You shouldn't drive"//OUTPUT=You shouldn't drive
         }
-        else if(isSober==false){
-            return "You shouldn't drive"
+        else if(isSober===false){// isSober=false
+            return "You shouldn't drive"//Output= You shouldn't drive
         }
-        else if(isTired==false && isSober==true){
+        else if(isTired===false && isSober===true){//isTired=true and isSober=true
             return "You can drive"
         }
     }
     else{
-        return "You cannot drive"
+        return "You can't drive"
     }
 }
 
