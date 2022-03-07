2d9afd91340e0b16379b1d60da4cae18a27070e2
Harisankar <harisankarsahoo58@gmail.com>
2022-03-07 20:27:23 +0530

    Playwith Array submission

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,17 +7,27 @@ function getEven(arr) {
 
         Write you code below
     */
+   count=arr.filter(num=>{
+       return num%2===0;
+
+   })
+   return count
 
 }
 
 function multiplyByN(arr, n) {
+
     /* 
         Return an array whose elements are multiplied by n
         Example
             Input: [1,3,4,55], 3
             Output: [3,9,13,165]
-        Write you code below
-    */
+        Write you code below*/
+    for (i=0; i<arr.length; i++){
+        let multi= arr[i]*n
+        arr[i] = multi
+    }
+    return arr;
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +38,12 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+   for(i=0; i<arr.length; i++){
+       if (i==n){
+           arr.splice(n,1)
+       }
+   }
+   return arr;
 }
 
 module.exports = {
