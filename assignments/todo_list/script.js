864dd6d843f23bca18d134e6462acf143deeeb84
Harisankar <harisankarsahoo58@gmail.com>
2022-03-16 20:37:46 +0530

    todo list actual api submission

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,16 +1,26 @@
-let Input = document.querySelector('input');
-let button = document.querySelector('button');
-let content = document.querySelector('.todos');
-let count =-1
-
-//main-part
-button.addEventListener('click',function(){
-    count +=1;
-    let P =document.createElement('p');   
-    P.innerHTML= Input.value
-    P.setAttribute("key",count);
-    content.appendChild(P);
-    P.addEventListener('click',function(){
-        content.removeChild(P);
-    })
-});
+let compBtn = document.querySelector('button#Comp');
+let pendBtn = document.querySelector('button#Pend');
+async function fetchtodo(){
+    let final=await fetch('https://jsonplaceholder.typicode.com/todos');//result
+    let data=await final.json();
+    let output='';
+    compBtn.onclick = function(){
+    data.forEach(function(todo,index) {
+        if (todo.completed == true){
+        output+=`<p key=${todo.id}>
+                ${todo.title}
+                </p>`
+}})
+    document.querySelector('.todos').innerHTML=output;
+    }
+    pendBtn.onclick = function(){
+        data.forEach(function(todo,index) {
+            if (todo.completed == false){
+                output+=`<p key=${todo.id}
+                ${todo.title}
+                </p>`
+    }})
+        document.querySelector('.todos').innerHTML=output;
+        }
+}
+    fetchtodo();
